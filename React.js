ec41d556080f1f7a38f435240f717d2005a7892c
yoswel badilla <113799193+Yoswell@users.noreply.github.com>
2022-10-24 15:34:09 -0600

    Initial commit

---
+++ React.js
@@ -0,0 +1,41 @@
+const input = document.querySelector(".agend-text");
+const agregarButton = document.querySelector(".submit");
+const list = document.querySelector("ul");
+
+agregarButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const text = input.value;
+
+  if (text !== "") {
+    const li = document.createElement("li");
+    li.className = "liCreated";
+    const p = document.createElement("p");
+    if(text.length > 20) {
+        alert("El texto es muy grande...");
+    } else {
+        p.textContent = text;
+
+        li.appendChild(p);
+        li.appendChild(addDeleteBtn());
+        list.appendChild(li);
+
+        input.value = "";
+    }
+  }
+});
+
+function addDeleteBtn() {
+    const deleteBtn = document.createElement("button");
+  
+    deleteBtn.textContent = "Eliminar";
+    deleteBtn.className = "btn-delete";
+  
+    deleteBtn.addEventListener("click", (e) => {
+      const item = e.target.parentElement;
+      list.removeChild(item);
+  
+    });
+  
+    return deleteBtn;
+  }
